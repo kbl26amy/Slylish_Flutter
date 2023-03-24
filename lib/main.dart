f7d89a0938de602c9dcb21b98c6c83fde362af58
Amy <amyyang@poseidongame.com>
2023-03-24 16:33:57 +0800

    1. 依照畫面寬度顯示三個或一個列表 2. 解掉IDE錯誤

--- lib/main.dart
+++ lib/main.dart
@@ -12,63 +12,62 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Stylish',
       theme: ThemeData(
-          colorScheme: ColorScheme.light(
+          //要將主題顏色改成白色，必須使用colorScheme
+          colorScheme: const ColorScheme.light(
               primary: Colors.white,
               onPrimary: Colors.white,
               onBackground: Colors.white,
               secondary: Colors.black)),
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
 
-class _MyHomePageState extends State<MyHomePage> {
-  void _incrementCounter() {
-    setState(() {});
-  }
-
-  @override
   //第一排圓角圖片
   Widget _topBanner(String assets) => Container(
-      margin: EdgeInsets.only(left: 5, top: 20, right: 5),
-      width: 400,
-      height: 200,
+      margin: const EdgeInsets.only(left: 5, top: 20, right: 5, bottom: 20),
+      width: 350,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8),
           image:
               DecorationImage(image: AssetImage(assets), fit: BoxFit.cover)));
   //衣服價目
-  Widget _clothesCard(String title, String price, String pic) =>
-      //使用Card跟Container，會讓裡面的圖片無法切圓角
-      Container(
-          height: 100,
-          width: 420,
-          child: Card(
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-                side: BorderSide(color: Colors.black38)),
-            child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-              Image.asset(pic),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  Text(price),
-                ],
-              ),
-            ]),
-          ));
+  Widget _clothesCard(String title, String price, String pic) => SizedBox(
+      height: 100,
+      width: 420,
+      child: Card(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+            side: const BorderSide(color: Colors.black38)),
+        child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Image.asset(pic),
+          Column(
+            //對齊文字上下置中、左右置左
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(title, style: const TextStyle(fontWeight: FontWeight.bold)),
+              Text(price),
+            ],
+          ),
+        ]),
+      ));
+
+  Widget _clothesList(String category) => Column(
+        children: [
+          Text(category, style: const TextStyle(fontWeight: FontWeight.bold)),
+          _clothesCard('UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
+          _clothesCard('UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
+          _clothesCard('UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
+          _clothesCard('UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
+          _clothesCard('UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
+        ],
+      );
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -82,7 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
           scrollDirection: Axis.vertical,
           children: [
             //為了設定高度用Container包起來
-            Container(
+            SizedBox(
               height: 200,
               //讓圖片可以往右滑動，把Row改成ListView
               child: ListView(scrollDirection: Axis.horizontal, children: [
@@ -95,20 +94,21 @@ class _MyHomePageState extends State<MyHomePage> {
                 _topBanner('assets/view.jpeg'),
               ]),
             ),
-            Column(
-              children: [
-                Text('女裝', style: TextStyle(fontWeight: FontWeight.bold)),
-                _clothesCard(
-                    'UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
-                _clothesCard(
-                    'UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
-                _clothesCard(
-                    'UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
-                _clothesCard(
-                    'UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
-                _clothesCard(
-                    'UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
-              ],
+            SizedBox(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: GridView.count(
+                //參照畫面寬度顯示三個或1個列表
+                crossAxisCount: (MediaQuery.of(context).orientation ==
+                        Orientation.landscape)
+                    ? 3
+                    : 1,
+                children: [
+                  _clothesList('女裝'),
+                  _clothesList('男裝'),
+                  _clothesList('配件')
+                ],
+              ),
             ),
           ],
         ));
