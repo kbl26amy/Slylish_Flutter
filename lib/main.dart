ab03a59dcaa24a2870f134188061fe7636fe8b00
Amy <amyyang@poseidongame.com>
2023-03-24 12:00:52 +0800

    1. 滑動衣服列表 2. 重命名部件 3. 對齊文字 4. 給畫面大小

--- lib/main.dart
+++ lib/main.dart
@@ -36,33 +36,39 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   //第一排圓角圖片
-  Widget _imageContainer(String assets) => Container(
+  Widget _topBanner(String assets) => Container(
       margin: EdgeInsets.only(left: 5, top: 20, right: 5),
       width: 400,
+      height: 200,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(8),
           image:
               DecorationImage(image: AssetImage(assets), fit: BoxFit.cover)));
-
-  Widget _buildDecoratedImage(String title, String price, String pic) =>
-      Expanded(
-        child: Card(
+  //衣服價目
+  Widget _clothesCard(String title, String price, String pic) =>
+      //使用Card跟Container，會讓裡面的圖片無法切圓角
+      Container(
+          height: 100,
+          width: 420,
+          child: Card(
             shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(18),
+                borderRadius: BorderRadius.circular(8),
                 side: BorderSide(color: Colors.black38)),
-            child: Row(children: [
+            child: Row(mainAxisAlignment: MainAxisAlignment.start, children: [
               Image.asset(pic),
               Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
                     title,
-                    style: TextStyle(fontWeight: FontWeight.w500),
+                    style: TextStyle(fontWeight: FontWeight.bold),
                   ),
                   Text(price),
                 ],
               ),
-            ])),
-      );
+            ]),
+          ));
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
@@ -73,22 +79,37 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
         body: ListView(
+          scrollDirection: Axis.vertical,
           children: [
             //為了設定高度用Container包起來
             Container(
               height: 200,
               //讓圖片可以往右滑動，把Row改成ListView
               child: ListView(scrollDirection: Axis.horizontal, children: [
-                _imageContainer('assets/view.jpeg'),
-                _imageContainer('assets/view.jpeg'),
-                _imageContainer('assets/view.jpeg'),
-                _imageContainer('assets/view.jpeg'),
-                _imageContainer('assets/view.jpeg'),
-                _imageContainer('assets/view.jpeg'),
-                _imageContainer('assets/view.jpeg'),
+                _topBanner('assets/view.jpeg'),
+                _topBanner('assets/view.jpeg'),
+                _topBanner('assets/view.jpeg'),
+                _topBanner('assets/view.jpeg'),
+                _topBanner('assets/view.jpeg'),
+                _topBanner('assets/view.jpeg'),
+                _topBanner('assets/view.jpeg'),
               ]),
             ),
-            _buildDecoratedImage('衣服標題', '價格', 'assets/clothes.jpeg')
+            Column(
+              children: [
+                Text('女裝', style: TextStyle(fontWeight: FontWeight.bold)),
+                _clothesCard(
+                    'UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
+                _clothesCard(
+                    'UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
+                _clothesCard(
+                    'UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
+                _clothesCard(
+                    'UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
+                _clothesCard(
+                    'UNIQLO特級極輕羽絨外套', 'NT\$323', 'assets/clothes.jpeg'),
+              ],
+            ),
           ],
         ));
   }
